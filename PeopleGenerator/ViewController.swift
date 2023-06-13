2c0ef2ebd8c111abff1c404a4173815f26a04bcb
Oguzhan Kabul <oguzhankabul@gmail.com>
2023-06-14 00:02:25 +0300

    Initial Commit

---
+++ PeopleGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeopleGenerator
+//
+//  Created by Oğuzhan Kabul on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
